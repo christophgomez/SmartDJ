dbbaa207c4e767b3c95fafb2f94c3402519b7db1
afryingpan <christopher.gomez.432@my.csun.edu>
2019-03-06 00:27:53 -0800

    frame detection with kinect/pylibfreenect

--- computer-vision/detection.py
+++ computer-vision/detection.py
@@ -47,7 +47,9 @@ while True:
 	frame = imutils.resize(frame, width=400)
 
 	# grab the frame dimensions and convert it to a blob
+	#print(frame);
 	(h, w) = frame.shape[:2]
+	#print(h,w);
 	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
 		0.007843, (300, 300), 127.5)
 
@@ -70,6 +72,7 @@ while True:
 			# the bounding box for the object
 			idx = int(detections[0, 0, i, 1])
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			print(box)
 			(startX, startY, endX, endY) = box.astype("int")
 
 			# draw the prediction on the frame
@@ -78,7 +81,7 @@ while True:
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
 				COLORS[idx], 2)
 			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y),
+			cv2.putText(frame, label, (5, 10),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
 	# show the output frame
